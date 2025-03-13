cbee9cce47ffb02e7439498c91a963af786a8c8a
MllHlg <79336756+MllHlg@users.noreply.github.com>
2025-03-13 20:42:00 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,33 +1,76 @@
 from ShapeBase import *
 import gymnasium as gym
 import random
+import matplotlib.pyplot as plt
+import time
+import os
 
-if __name__ == '__main__':
-
-    # Quatre formes disponibles pour l'instant
+def run(epsilon, alpha, gamma, episodes: int = 100) -> None:
     env = ShapeEnv(Shape(1))
-    episodes = 100
-    epsilon = 0.1
-    alpha = 0.5
-    gamma = 0.2
-    for e in range(episodes) :
-        if e > 0 : env.reset()
+
+    rewards_per_episodes = np.zeros(episodes)
+
+    for ep in range(episodes):
+        print(f"Épisode : {ep + 1} / {episodes}") # Info sur l'avancé des épisodes
+        
+        if ep > 0 : env.reset() # Reset de l'environnement
+
         action = env.get_random_action(epsilon)
+
+        rewards = 0
+
         while not env.terminated:
             state = tuple(sorted(env.state))
             point = point_coordinate(action[0])
             reward = env.step(action)
             next_state = tuple(sorted(env.state))
             next_action = env.get_random_action(epsilon)
+
             if not env.terminated:
                 env.shape.Q[state][(point, action[2])] += alpha * (reward + gamma * env.shape.Q[next_state][(point_coordinate(next_action[0]), next_action[2])] - env.shape.Q[state][(point, action[2])])
                 action = next_action
-            else :
+            else:
                 env.shape.Q[state][(point, action[2])] += alpha * (reward - env.shape.Q[state][(point, action[2])])
+            
+            rewards += reward
+        
+        rewards_per_episodes[ep] = rewards
+
         env.render()
     env.afficheEtat()
 
+    # Sauvegarde des courbes
+    time_str = time.strftime("%Y%m%d-%H%M%S")
+    
+    if not os.path.exists("./results"):
+        os.makedirs('./results')
+    os.makedirs(f'./results/{time_str}')
 
+    # Calcul de la moyenne des rewards obtenus
+    mean_rewards = np.zeros(episodes)
+    for i in range(episodes):
+        mean_rewards[i] = np.mean(rewards_per_episodes[max(0, i-100):(i+1)])
+
+    #plt.plot(rewards_per_episodes, 'b-') # Courbe Bleu : Reward Brut
+    plt.plot(mean_rewards, 'r-') # Courbe Rouge : Moyenne des rewards
+    plt.savefig(f'./results/{time_str}/rpe.png')
+    plt.close()
+
+    plt.plot(env.shape.nb_visits, 'b+') # Affiche le tableau des visites
+    plt.savefig(f'./results/{time_str}/nb_visite.png')
+    plt.close()
+
+    plt.plot(env.shape.array_rewards, 'b+') # Affiche le tableau des visites
+    plt.savefig(f'./results/{time_str}/array_reward.png')
+    plt.close()
+
+    return np.mean(mean_rewards)
+
+
+if __name__ == '__main__':
+    
+    run(0.1,0.1,0.6, 1000)
+    
     ## Cut from point 13, face 2 in direction 3 (east)
     #cut(13, 2, 3)
     #print_infos()
